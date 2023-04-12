7554cbba6ec9ce22046783b7d799605fd5133a20
{alexius <{alexiussavage0@gmail.com}>
2023-04-12 14:12:04 -0700

    deleting extra files

--- script.js
+++ script.js
@@ -2,30 +2,29 @@ let questions=[
     {
     id:1,
     question: 'Question 1: Commonly used data types DO NOT include:',
-    option: ['strings','booleans','numbers','alerts'],
+    options: ['strings','booleans','numbers','alerts'],
     correctAnswer: 3,
     },
     {
     id:2,
     question: 'Question 2: The condition in an if/else statement is enclosed with?',
-    option: ['quotes','curly brackets','parenthesis','square brackets'],
+    options: ['quotes','curly brackets','parenthesis','square brackets'],
     correctAnswer: 1,
     },
     {
     title: 'Question 3: Arrays in Javascript can be used to store',
-    option: ['numbers and strings','other arrays','booleans','all of the above'],
+    options: ['numbers and strings','other arrays','booleans','all of the above'],
     correctAnswer: 3,
     },
     {
     title: 'Question 4: String values must be enclosed within ______ when being assigned to variables',
-    option: ['commas','curly brackets','quotes','parenthesis'],
+    options: ['commas','curly brackets','quotes','parenthesis'],
     correctAnswer: 2,
     },
     {
     title: 'Question 5: A very useful tool used during development and debugging for printing content to the debugger is?',
-    option: ['Javascript','Terminal/Bash','For loops','Console.log'],
+    options: ['Javascript','Terminal/Bash','For loops','Console.log'],
     correctAnswer: 3
     }
 ]
 
-    

58746d8f1919e0242483db97de62600a93106f54
{alexius <{alexiussavage0@gmail.com}>
2023-04-12 15:33:30 -0700

    added more javascript

---
+++ user.js
@@ -0,0 +1,9 @@
+const start_button = document.querySelector('.start button');
+const start_quiz_page = document.querySelector('.start_quiz_page')
+const exit_button = buttons.querySelector('.button', 'exit')
+const quiz = document.querySelector('.quiz')
+const results_box = document.querySelector('.results_box')
+const options_list = document.querySelector('.options_list')
+const time_line = document.querySelector('header','.time_line')
+const timeCount = document.querySelector('.timer','.timer_seconds')
+

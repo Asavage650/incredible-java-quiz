d6eeaa280b7fa6567c4f8f21b16d9cdcadbb1a77
{alexius <{alexiussavage0@gmail.com}>
2023-04-13 14:45:29 -0700

    debugging still

--- questions.js
+++ questions.js
@@ -1,31 +1,31 @@
 let questions=[
     {
     id:1,
-    question: 'Question 1: Commonly used data types DO NOT include:',
+    question: ' Commonly used data types DO NOT include:',
     options: ['strings','booleans','numbers','alerts'],
     correctAns: 3,
     },
     {
     id:2,
-    question: 'Question 2: The condition in an if/else statement is enclosed with?',
+    question: ' The condition in an if/else statement is enclosed with?',
     options: ['quotes','curly brackets','parenthesis','square brackets'],
     correctAns: 1,
     },
     {
     id:3,
-    question: 'Question 3: Arrays in Javascript can be used to store',
+    question: ' Arrays in Javascript can be used to store',
     options: ['numbers and strings','other arrays','booleans','all of the above'],
     correctAns: 3,
     },
     {
     id: 4,
-    question: 'Question 4: String values must be enclosed within ______ when being assigned to variables',
+    question: ' String values must be enclosed within ______ when being assigned to variables',
     options: ['commas','curly brackets','quotes','parenthesis'],
     correctAns: 2,
     },
     {
     id: 5,
-    question: 'Question 5: A very useful tool used during development and debugging for printing content to the debugger is?',
+    question: ' A very useful tool used during development and debugging for printing content to the debugger is?',
     options: ['Javascript','Terminal/Bash','For loops','Console.log'],
     correctAns: 3
     }
